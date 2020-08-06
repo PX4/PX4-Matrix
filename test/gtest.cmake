ccc929fe755e8f8af32759b3f001df22972cfc6e
kamilritz <kritz@ethz.ch>
2020-08-06 17:47:43 +0200

    try to fix ci

--- test/gtest.cmake
+++ test/gtest.cmake
@@ -1,3 +1,5 @@
+set_directory_properties(PROPERTIES COMPILE_OPTIONS "")
+
 # Download and unpack googletest at configure time
 configure_file(${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.in googletest-download/CMakeLists.txt)
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" . RESULT_VARIABLE result1 WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download)
@@ -7,5 +9,4 @@ if(result1 OR result2)
 endif()
 
 # Add googletest, defines gtest and gtest_main targets
-set_directory_properties(PROPERTIES COMPILE_OPTIONS "")
 add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/googletest-src ${CMAKE_CURRENT_BINARY_DIR}/googletest-build EXCLUDE_FROM_ALL)
