6c550f097fac4486f25c1ab24629d948810f472c
Daniel Agar <daniel@agar.ca>
2020-12-15 21:08:27 -0500

    delete old MATRIX_GCC_4_8_WORKAROUND

--- matrix/Matrix.hpp
+++ matrix/Matrix.hpp
@@ -18,14 +18,6 @@
 
 #include "math.hpp"
 
-// There is a bug in GCC 4.8, which causes the compiler to segfault due to array {} constructors.
-// Do for-loop constructors just for GCC 4.8
-#ifdef __GNUC__
-#define MATRIX_GCC_4_8_WORKAROUND (__GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 9))
-#else
-#define MATRIX_GCC_4_8_WORKAROUND 0
-#endif
-
 namespace matrix
 {
 
@@ -41,27 +33,12 @@ class Slice;
 template<typename Type, size_t M, size_t N>
 class Matrix
 {
-#if MATRIX_GCC_4_8_WORKAROUND
-    Type _data[M][N];
-#else
     Type _data[M][N] {};
-#endif
 
 public:
 
     // Constructors
-#if MATRIX_GCC_4_8_WORKAROUND
-    Matrix()
-    {
-        for (size_t i = 0; i < M; i++) {
-            for (size_t j = 0; j < N; j++) {
-                _data[i][j] = Type{};
-            }
-        }
-    }
-#else
     Matrix() = default;
-#endif
 
     explicit Matrix(const Type data_[M*N])
     {
